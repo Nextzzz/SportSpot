44a0f986adf8a1bc7a7ead35509e24f59205becb
yarikpo <yaroslavpopovich04@gmail.com>
2023-10-21 01:00:10 +0300

    feature: created empty product page

---
+++ client/src/pages/Products/components/Products.jsx
@@ -0,0 +1,18 @@
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import '../styles/products.css'
+const Products = () => {
+
+    return (
+        <Box component="main" sx={{ p: 3 }}>
+            <Toolbar />
+            <Typography>
+                PRODUCTS
+            </Typography>
+            <h1>PRODUCTS</h1>
+        </Box>
+    )
+}
+
+export default Products;
